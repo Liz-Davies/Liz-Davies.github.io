3060f4614b6179c555f91f3bbf5b3843c3f6c4a5
Liz Davies <sarah.e.e.davies@gmail.com>
2020-04-18 18:13:35 -0400

    test react version

---
+++ test/precache-manifest.3facbe28f43dcead49e7decb7ae4681c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca5819f9df1529b60949521353145ac",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "b71cad453883089d33da",
+    "url": "/test/static/css/main.c0ceb221.chunk.css"
+  },
+  {
+    "revision": "ba7c5d58c48f808f2bc5",
+    "url": "/test/static/js/2.b80e57a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.b80e57a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71cad453883089d33da",
+    "url": "/test/static/js/main.dbdfbef8.chunk.js"
+  },
+  {
+    "revision": "b3a5b4bc88f28d347cf1",
+    "url": "/test/static/js/runtime-main.e0e1541c.js"
+  },
+  {
+    "revision": "f70de74b0e2405c3315c8c69dae14e89",
+    "url": "/test/static/media/TileableWatermelon2.f70de74b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
